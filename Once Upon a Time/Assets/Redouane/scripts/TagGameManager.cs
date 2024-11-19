5965f3bc523d9dc278964393a87388f3a8a1936e
solredk <144345763+solredk@users.noreply.github.com>
2024-11-19 12:30:36 +0100

    ik heb een scoreboard gemaakt textmechpro toegevoegd en nog wat bugs gefixed

--- Once Upon a Time/Assets/Redouane/scripts/TagGameManager.cs
+++ Once Upon a Time/Assets/Redouane/scripts/TagGameManager.cs
@@ -3,68 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-    enum GameState
-    {
-        preparelevel,
-        StartGame,
-        CollectData,
-        Endgame,
-        PauzeGame
-    }
 
-public class TagGameManager : MonoBehaviour
+public class TagGameManager : GameManager
 {
-    [SerializeField] PlayerInputManager playerInputManager;
+    private bool taggerChosen=false;
+    private Tag tagScript;
 
-    [SerializeField] List<GameObject> Players; [Tooltip("de default player prefaps (max 4)")]
-    [SerializeField] List<GameObject> currentPlayers; [Tooltip("de prefaps in de scene (max 4)")]
-
-    [Header("de state waar de game in zit")]
-    [SerializeField] GameState state;
-
-    [Header ("de counters")]
-    [SerializeField]float Gameduration;
-    [SerializeField]float counter; [Tooltip("de timer voor de start van de game en de game zelf")]
-
-    [Header("de player nummers")]
-    [SerializeField] int randomTaggerIndex; [Tooltip("de random nummer die wordt gegenereerd aan de start van de game om de player voor de stellen")]
-    [SerializeField] int playerInt;
-    [SerializeField] int playercount;
-   
-    private void Start()
+    protected override void PrepareLevel()
     {
-        if (playerInputManager != null)
-            playerInputManager = GetComponent<PlayerInputManager>();
-    }
-
-    private void Update()
-    {
-
-        switch (state)
-        {
-            case GameState.preparelevel:
-                PrepareLevel();
-                break;
-            case GameState.StartGame:
-                StartGame();
-                break;
-            case GameState.CollectData:
-                CollectData();
-                break;
-            case GameState.Endgame:
-                break;
-                
-        }
-    }
-    
-    private void PrepareLevel()
-    {
-        playerInputManager.EnableJoining();
-        
-        if (playerInputManager.playerCount >= 1)
+        base.PrepareLevel();
+        if (playerInputManager.playerCount >= 2)
         {
             counter += Time.deltaTime;
-             if (counter > 10)
+            if (counter > 10)
             {
                 state = GameState.StartGame;
                 playerInputManager.DisableJoining();
@@ -81,39 +32,25 @@ public class TagGameManager : MonoBehaviour
         }
     }
 
-    private void StartGame()
+    protected override void StartGame()
     {
-        currentPlayers[randomTaggerIndex].GetComponent<Tag>().Getaged();
-
-        counter += Time.deltaTime;
-        if (counter > Gameduration)
+        if (taggerChosen == false)
         {
-             state = GameState.CollectData;
+            currentPlayers[randomTaggerIndex].GetComponent<Tag>().Getaged();
+            taggerChosen = true;
         }
+        base.StartGame();
     }
 
-    private void CollectData()
+    protected override void CollectData()
     {
-       foreach (GameObject player in currentPlayers)
-             {
-               playerInt++;
-               PlayerPrefs.SetFloat("player" + playerInt, player.GetComponent<Tag>().ReturnSurviveTime());
-               float playerScore = PlayerPrefs.GetFloat("player" + playerInt);
-               Debug.Log(playerScore);
-               Debug.Log(PlayerPrefs.GetInt("player" + playerInt));
-             }
-       playerInt = playercount;
-       state = GameState.Endgame;
-    }
-
-    public void DoPauze(InputAction.CallbackContext context)
-    {
-        Time.timeScale = 0f;
-
-    }
-    public void DoJoin()
-    {
-        playerInputManager.playerPrefab = Players[playercount];
-        playercount++;
+        foreach (GameObject player in currentPlayers)
+        {
+            tagScript = player.GetComponent<Tag>();
+            PlayerPrefs.SetFloat(tagScript.username, tagScript.ReturnSurviveTime());
+            float playerScore = PlayerPrefs.GetFloat(tagScript.username);
+            Debug.Log(tagScript.username + playerScore);
+        }
+        base.CollectData();
     }
 }
