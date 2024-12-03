22ba6ebf0f1158813127efb37a64c8ec36d4461a
Jesse3215 <jessefaassen20073215@gmail.com>
2024-12-03 14:32:09 +0100

    snel

--- Once Upon a Time/Assets/Jesse/Scripts/Hinkel.cs
+++ Once Upon a Time/Assets/Jesse/Scripts/Hinkel.cs
@@ -8,102 +8,25 @@ using UnityEngine.UI;
 
 public class Hinkel : MonoBehaviour
 {
-    private NieuweHinkel NieuweHinkel;
+    [SerializeField] private List<NieuweHinkel> Nieuwehinkel;
 
-    public List<GameObject> players; 
-    public List<Image> keyImages;
-    private List<Image> randomImage1;
-    private List<Image> randomImage2;
-
-    private float randomTime1;
-    private float randomTime2;
+    public float randomTime1;
+    public float randomTime2;
 
     void Start()
     {
         randomTime1 = Random.Range(2f, 4f);
         randomTime2 = Random.Range(0.5f, 3f);
 
-        foreach (Image image in keyImages)
-        {
-            image.enabled = false;
-        }
-
-        //StartCoroutine(StartGame());
+        StartCoroutine(StartGame());
     }
 
-    List<Image> GetRandomImages(List<Image> inputList, int count)
+    private IEnumerator StartGame()
     {
-        List<Image> outputList = new List<Image>();
-
-        for (int i = 0; i < count; i++)
+        yield return new WaitForSeconds(1f);
+        for(int i = 0; i < Nieuwehinkel.Count; i++)
         {
-            int index = Random.Range(0, inputList.Count);
-            outputList.Add(inputList[index]);
+            Nieuwehinkel[i].StartMove();
         }
-        return outputList;
-    }
-
-    public IEnumerator ShowImage1()
-    {
-        yield return new WaitForSeconds(randomTime1);
-        randomImage1 = GetRandomImages(keyImages, 1);
-        randomImage1[0].enabled = true;
-
-        while (randomImage1[0].enabled)
-        {
-            CheckKeyPress1();
-            yield return null;
-        }
-    }
-
-    public IEnumerator ShowImage2()
-    {
-        yield return new WaitForSeconds(randomTime2);
-        randomImage2 = GetRandomImages(keyImages, 2);
-        randomImage2[0].enabled = true;
-        randomImage2[1].enabled = true;
-
-        while (randomImage2[0].enabled && randomImage2[1].enabled)
-        {    
-            CheckKeyPress2();
-            yield return null;
-        }
-    }
-
-    //private IEnumerator StartGame()
-    //{
-    //    yield return new WaitForSeconds(1f);
-    //    NieuweHinkel.StartMove();
-    //}
-
-    private void CheckKeyPress1()
-    {
-        Debug.Log("CheckingkeyPress1");
-        foreach(var keyImages in NieuweHinkel.keyMapping1)
-        {
-            if(Input.GetKeyDown(keyImages.Key) && keyImages.Value.enabled)
-            {
-                keyImages.Value.enabled = false;
-                NieuweHinkel.MoveToNextLocation();
-                break;
-            }
-        }
-    }
-
-    private void CheckKeyPress2()
-    {
-        var keysPressed = NieuweHinkel.keyMapping1
-            .Where(kv => Input.GetKey(kv.Key) && kv.Value.enabled)
-            .ToList();
-
-        if (keysPressed.Count == 2)
-        {
-            foreach (var keyImage in keysPressed)
-            {
-                keyImage.Value.enabled = false;
-            }
-
-            NieuweHinkel.MoveToNextLocation();
-        }
-    }
+    }    
 }
--- Once Upon a Time/Assets/Jesse/Scripts/NieuweHinkel.cs
+++ Once Upon a Time/Assets/Jesse/Scripts/NieuweHinkel.cs
@@ -11,6 +11,9 @@ public class NieuweHinkel : MonoBehaviour
 
     [SerializeField] public List<Transform> nextLocations;
     [SerializeField] public List<Image> keyImages;
+    [SerializeField] public List<KeyCode> keysCodes;
+    private List<Image> randomImage1;
+    private List<Image> randomImage2;
 
     public Dictionary<KeyCode, Image> keyMapping1;
 
@@ -19,6 +22,8 @@ public class NieuweHinkel : MonoBehaviour
     private void Start()
     {
         hinkel = FindAnyObjectByType<Hinkel>();
+
+        ResetImages();
 
         keyMapping1 = new Dictionary<KeyCode, Image>
         {
@@ -29,13 +34,10 @@ public class NieuweHinkel : MonoBehaviour
             { KeyCode.T, keyImages[4] },
             { KeyCode.Y, keyImages[5] }
         };
-
-        StartCoroutine(StartMove(1f));
     }
 
-    public IEnumerator StartMove(float delay)
+        public void StartMove()
     {
-        yield return new WaitForSeconds(delay);
         playerPos.transform.position = Vector3.MoveTowards(playerPos.transform.position, nextLocations[1].position, speed);
         nextLocations.Remove(nextLocations[0]);
     }
@@ -54,18 +56,94 @@ public class NieuweHinkel : MonoBehaviour
         if (other.gameObject.CompareTag("1 vakje"))
         {
             Debug.Log("in1");
-            StartCoroutine(hinkel.ShowImage1());
+            StartCoroutine(ShowImage1());
         }
 
         if (other.gameObject.CompareTag("2 vakjes"))
         {
             Debug.Log("in2");
-            StartCoroutine(hinkel.ShowImage2());
+            StartCoroutine(ShowImage2());
         }
 
         if (other.gameObject.CompareTag("Finish"))
         {
             Debug.Log("Won!");
+        }
+    }
+
+    List<Image> GetRandomImages(List<Image> inputList, int count)
+    {
+        List<Image> outputList = new List<Image>();
+
+        for (int i = 0; i < count; i++)
+        {
+            int index = Random.Range(0, inputList.Count);
+            outputList.Add(inputList[index]);
+        }
+        return outputList;
+    }
+
+    public IEnumerator ShowImage1()
+    {
+        yield return new WaitForSeconds(hinkel.randomTime1);
+        randomImage1 = GetRandomImages(keyImages, 1);
+        randomImage1[0].enabled = true;
+
+        while (randomImage1[0].enabled)
+        {
+            CheckKeyPress1();
+            yield return null;
+        }
+    }
+
+    public IEnumerator ShowImage2()
+    {
+        yield return new WaitForSeconds(hinkel.randomTime2);
+        randomImage2 = GetRandomImages(keyImages, 2);
+        randomImage2[0].enabled = true;
+        randomImage2[1].enabled = true;
+
+        while (randomImage2[0].enabled && randomImage2[1].enabled)
+        {
+            CheckKeyPress2();
+            yield return null;
+        }
+    }
+
+    private void CheckKeyPress1()
+    {
+        Debug.Log("CheckingkeyPress1");
+        for (int i = 0; i < keyImages.Count; i++)
+        {
+            if (Input.GetKeyDown(keysCodes[i]) && keyImages[i].enabled)
+            {
+                ResetImages();
+                MoveToNextLocation();
+                break;
+            }
+        }
+    }
+
+    private void CheckKeyPress2()
+    {
+        for (int i = 0; i < keyImages.Count; i++)
+        {
+            for (int j = 0; j < keyImages.Count; j++)
+            {
+                if (Input.GetKeyDown(keysCodes[i]) && keyImages[i].enabled && Input.GetKeyDown(keysCodes[j]) && keyImages[j].enabled)
+                {
+                    ResetImages();
+                    MoveToNextLocation();
+                    break;
+                }
+            }
+        }
+    }
+    void ResetImages()
+    {
+        for (int i = 0; i < keyImages.Count; i++)
+        {
+            keyImages[i].enabled = false;
         }
     }
 }
